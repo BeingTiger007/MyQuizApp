4d84776fd5c917bf33fcf195421c7a75eb90679d
BeingTiger007 <aslam.tiger.9047@gmail.com>
2020-02-04 11:56:45 +0530

    updated about us page

--- app/src/main/java/com/example/myquizapp/About.java
+++ app/src/main/java/com/example/myquizapp/About.java
@@ -2,6 +2,7 @@ package com.example.myquizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 
 public class About extends AppCompatActivity {
@@ -10,5 +11,11 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.colorWhite, this.getTheme()));
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.colorWhite));
+        }
     }
 }
